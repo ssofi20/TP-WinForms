476712fdb3a55c1a89a09f173aebae60a2def9bf
Sofi19 <bi40725@gmail.com>
2025-09-05 18:23:36 -0300

    Clases

---
+++ WindowsFormsApp1/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Articulo
+    {
+        public int IDArticulo { get; set; }
+        public string NombreArt { get; set; }
+        public string DescripcionArt { get; set; }
+        public decimal PrecioArt { get; set; }
+        public Categoria CategoriaArt { get; set; }
+        public Marca MarcaArt { get; set; }
+        public Imagen ImagenArt { get; set }    
+    }
+
+}
---
+++ WindowsFormsApp1/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Categoria
+    {
+        public int IDCategoria { get; set; }
+
+        public string NombreCategoria { get; set; }
+    }
+}
